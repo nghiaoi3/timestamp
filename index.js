529773cc09786af050eae8d14bc610dace0bf2d7
le nghia <tro.ngnghia@yahoo.com.vn>
2017-05-21 11:32:49 +0000

    sssss

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var express = require('express')
 var app = express()
 var port = process.env.PORT || 3000
 
+app.get('/', function(req, res) {
+    res.send('Hello from NGHIA, please enter timestamp in your URL query');
+});
+
 app.get('/:timestamp', function(req,res){
     var timestamp = req.params.timestamp;
     res.json(getJson(timestamp));
